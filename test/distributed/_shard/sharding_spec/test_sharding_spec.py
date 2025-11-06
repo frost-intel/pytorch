71c57aa20a03c39a80887df3cf7b34f49c544af2
Zeng, Xiangdong <xiangdong.zeng@intel.com>
2025-11-06 17:14:47 +0800

    test/distributed/_shard/sharding_spec/test_sharding_spec.py

--- test/distributed/_shard/sharding_spec/test_sharding_spec.py
+++ test/distributed/_shard/sharding_spec/test_sharding_spec.py
@@ -45,7 +45,7 @@ device_type = (
 )
 
 class TestShardingSpec(TestCase):
-    # @skip_but_pass_in_sandcastle_if(not TEST_MULTIGPU, "2 CUDA GPUs are needed")
+    @skip_but_pass_in_sandcastle_if(not TEST_MULTIGPU, "2 CUDA GPUs are needed")
     def test_device_placement(self):
         # valid devices
         DevicePlacementSpec(f"{device_type}:0")
@@ -65,7 +65,7 @@ class TestShardingSpec(TestCase):
         with self.assertRaisesRegex(RuntimeError, "Invalid device string"):
             DevicePlacementSpec("rank:0/cpu2")
 
-    # @skip_but_pass_in_sandcastle_if(not TEST_MULTIGPU, "2 CUDA GPUs are needed")
+    @skip_but_pass_in_sandcastle_if(not TEST_MULTIGPU, "2 CUDA GPUs are needed")
     def test_chunked_sharding_spec(self):
         # Test valid specs.
         ChunkShardingSpec(0, [torch.device(0), torch.device(1)])
@@ -98,7 +98,7 @@ class TestShardingSpec(TestCase):
         with self.assertRaisesRegex(RuntimeError, "Invalid device string"):
             ChunkShardingSpec(0, [f"rank:0/{device_type}:foo", f"{device_type}:1"])
 
-    # @skip_but_pass_in_sandcastle_if(not TEST_MULTIGPU, "2 CUDA GPUs are needed")
+    @skip_but_pass_in_sandcastle_if(not TEST_MULTIGPU, "2 CUDA GPUs are needed")
     def test_enumerable_sharding_spec(self):
         # test valid specs
 
